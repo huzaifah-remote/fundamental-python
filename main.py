bf8d22bbd8b650861ff6498a2c28dad23d55ff2f
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-01-10 09:33:28 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 
-print ("hello word")
+print("hello word")
 
 try:
-    r = requests.get ('https://google.com')
-    print (r.status_code)
-    if r.status_code ==200:
+    r = requests.get('https://google.com')
+    print(r.status_code)
+    if r.status_code == 200:
         print(r.text)
 except Exception as e:
     print('Ada error', e)
---
+++ main2.py
@@ -0,0 +1,6 @@
+#KONSTRUKSI DASAR PYTHON
+#SEQUENTIAL
+print("Hello World!")
+print("by Ujeb")
+print("Tanggal 10 Januari 2021")
+print("-"*10)
