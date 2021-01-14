f8a3242ba59dfb97fdd95bbf7e6c313978e94080
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-01-14 17:13:22 +0700

    initial commit

--- main2.py
+++ main2.py
@@ -1,6 +1,19 @@
-#KONSTRUKSI DASAR PYTHON
-#SEQUENTIAL
-print("Hello World!")
-print("by Ujeb")
-print("Tanggal 10 Januari 2021")
-print("-"*10)
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL: Eksekusi berurutan
+print('Hello World!')
+print('by Ujeb')
+print('Tanggal 10 Januari 2021')
+print('-' * 20)
+
+#PERCABANGAN: Eksekusi terpilih
+ingin_cepat = False
+if ingin_cepat:
+    print('Jalan lurus aja ya!')
+else:
+    print('jalan lain')
+
+#PERULANGAN
+jumlah_anak = 4
+
+for index_anak in range(1, jumlah_anak+1): #jumlah perulangan = 5 - 1 = 4
+    print(f'Halo anak {index_anak}')
